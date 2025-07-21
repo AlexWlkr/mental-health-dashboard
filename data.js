77078207b1cb751e608ceb352407f1368b18272e
Alex Walker <walkernalex@gmail.com>
2025-07-21 14:24:58 -0400

    feat: add mock program data to data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const programs = [
+    {
+        name: "Healing Together",
+        type: "Therapy",
+        audience: "BIPOC",
+        accessibility: "Virtual"
+    },
+    {
+    name: "Baltimore Peer Circle",
+    type: "Peer Support",
+    audience: "College Students",
+    accessibility: "In-Person"
+  },
+  {
+    name: "Youth Crisis Text Line",
+    type: "Hotline",
+    audience: "Youth",
+    accessibility: "Virtual"
+  }
+];
