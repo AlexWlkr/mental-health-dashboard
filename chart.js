cc2189f1381fa94ea37698ef29a93bc8b5d4fc45
Alex Walker <walkernalex@gmail.com>
2025-07-23 16:58:05 -0400

    add: create all 3 bar charts

--- chart.js
+++ chart.js
@@ -3,16 +3,51 @@ const typeCtx = document.getElementById('programTypeChart').getContext('2d');
 const typeLabels = ['Peer Support', 'Hotline', 'Therapy', 'Workshop'];
 const typeData = [7, 2, 5, 0];
   
-
-
+new Chart(typeCtx, {
+type: 'bar',
+ data: {
+labels: typeLabels, datasets: [{
+label: 'Number of Programs by Type',
+ data: typeData,
+  backgroundColor: '#82b1ff',
+  borderColor: '#2962ff',
+  borderWidth: 1    
+ }]
+  },
+  });
 
 // chart 2: programs by audience
 const typeCtx = document.getElementById('programAudienceChart').getContext('2d');
 const typeLabels = ['Adult', 'Family', 'Youth', 'LGBTQ', 'BIPOC', 'Teens', 'Collegestudents'];
 const typeData = [4, 5, 1, 3, 0, 1, 0];
 
+new Chart(typeCtx, {
+type: 'bar',
+ data: {
+labels: typeLabels, datasets: [{
+label: 'Number of Programs by Audience',
+ data: typeData,
+  backgroundColor: '#82b1ff',
+  borderColor: '#2962ff',
+  borderWidth: 1    
+ }]
+  },
+  });
 
 // chart 3: programs by accessiblitity options
  const typeCtx = document.getElementById('programAccesibilityChart').getContext('2d');
 const typeLabels = ['ASL Interpretation Offered', 'Services Offered in Spanish', 'Virtual', 'In-Person', 'Hybrid', 'Service Animal Friendly', 'Wheelchair Accessible Location'];
 const typeData = [1, 1, 2, 4, 6, 0, 0];
+
+new Chart(typeCtx, {
+type: 'bar',
+ data: {
+labels: typeLabels, datasets: [{
+label: 'Number of Programs by Accessibility Options',
+ data: typeData,
+  backgroundColor: '#82b1ff',
+  borderColor: '#2962ff',
+  borderWidth: 1    
+  }]
+  },
+  });
