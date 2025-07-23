04a62aee6fe17dfce69bec8015c169643f91bb47
Alex Walker <walkernalex@gmail.com>
2025-07-23 16:52:52 -0400

    feat: Define labels and data in new chart.js file

---
+++ chart.js
@@ -0,0 +1,18 @@
+// chart 1: programs by type
+const typeCtx = document.getElementById('programTypeChart').getContext('2d');
+const typeLabels = ['Peer Support', 'Hotline', 'Therapy', 'Workshop'];
+const typeData = [7, 2, 5, 0];
+  
+
+
+
+// chart 2: programs by audience
+const typeCtx = document.getElementById('programAudienceChart').getContext('2d');
+const typeLabels = ['Adult', 'Family', 'Youth', 'LGBTQ', 'BIPOC', 'Teens', 'Collegestudents'];
+const typeData = [4, 5, 1, 3, 0, 1, 0];
+
+
+// chart 3: programs by accessiblitity options
+ const typeCtx = document.getElementById('programAccesibilityChart').getContext('2d');
+const typeLabels = ['ASL Interpretation Offered', 'Services Offered in Spanish', 'Virtual', 'In-Person', 'Hybrid', 'Service Animal Friendly', 'Wheelchair Accessible Location'];
+const typeData = [1, 1, 2, 4, 6, 0, 0];
