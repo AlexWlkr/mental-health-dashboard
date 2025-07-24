fea6218e16e1cdb61167e7fde530cf02f39f7a7e
Alex Walker <walkernalex@gmail.com>
2025-07-24 09:25:47 -0400

    add: alt text to img for accessibility standards

--- data.js
+++ data.js
@@ -4,97 +4,111 @@ const programs = [
     type: "Peer Support",
     audience: "Adults",
     accessibility: "Hybrid",
-    image: "./assets/nami-adults.jpg"
+    image: "./assets/nami-adults.jpg", 
+    altText: "Adult mental health service in Baltimore"
   },
   {
     name: "NAMI Metro Baltimore – Peer-led Support Groups",
     type: "Peer Support",
     audience: "Family",
     accessibility: "Hybrid",
-    image: "./assets/nami-family.jpg"
+    image: "./assets/nami-family.jpg",
+     altText: "Family mental health service in Baltimore"
   },
   {
     name: "NAMI Metro Baltimore – Peer-led Support Groups",
     type: "Peer Support",
     audience: "Youth",
     accessibility: "Hybrid",
-    image: "./assets/nami-youth.jpg"
+    image: "./assets/nami-youth.jpg",
+     altText: "Youth mental health service in Baltimore"
   },
   {
     name: "NAMI Metro Baltimore – LGBTQIA+ Resources & Support",
     type: "Peer Support",
     audience: "LGBTQ",
     accessibility: "Hybrid",
-   image: "./assets/nami-peer-lgbtq.jpg"
+   image: "./assets/nami-peer-lgbtq.jpg",
+    altText: "LGBTQ mental health service in Baltimore"
   },
   {
     name: "Baltimore County Health – Mobile Crisis and Urgent Care",
     type: "Hotline",
     audience: "Adults",
     accessibility: "In-Person",
-   image: "./assets/hotline.jpg"
+   image: "./assets/hotline.jpg",
+    altText: "Adult mental health hotline service in Baltimore"
   },
   {
     name: "Baltimore County Health – Mobile Crisis and Urgent Care",
     type: "Hotline",
     audience: "Family",
     accessibility: "In-Person",
-    image: "./assets/hotline-2.jpg"
+    image: "./assets/hotline-2.jpg",
+     altText: "Family mental health hotline service in Baltimore"
   },
   {
     name: "Hearts & Ears, Inc. (Y2Connect)",
     type: "Peer Support",
     audience: "LGBTQ",
     accessibility: "Hybrid",
-    image: "./assets/hearts-and-ears.jpg"
+    image: "./assets/hearts-and-ears.jpg",
+     altText: "LGBTQ peer support mental health service in Baltimore"
   },
   {
     name: "Free Baltimore",
     type: "Therapy",
     audience: "LGBTQ",
     accessibility: "In-Person",
-    image: "./assets/free-baltimore.jpg"
+    image: "./assets/free-baltimore.jpg",
+     altText: "LGBTQ mental health service in Baltimore"
   },
   {
     name: "Mighty Behavioral Health Services",
     type: "Therapy",
     audience: "Adults",
     accessibility: "Virtual",
-    image: "./assets/mightyjpg"
+    image: "./assets/mightyjpg",
+     altText: "Adult virtual therapy in Baltimore"
   },
   {
     name: "Talkspace Go via Baltimore County Schools",
     type: "Therapy",
     audience: "Teens",
     accessibility: "Virtual",
-    image: "./assets/bcps-talkspacejpg"
+    image: "./assets/bcps-talkspacejpg",
+     altText: "Therapy service for teens in Baltimore"
   },
   {
     name: "Overcare Pyschiatry and Therapy",
     type: "Therapy",
     audience: "Family",
     accessibility: "In-Person",
-    image: "./assets/overcare.jpg"
+    image: "./assets/overcare.jpg",
+     altText: "Family in-person therapy in Baltimore"
   },
   {
     name: "BHSB – ASL-Accessible Services",
     type: "Peer Support",
     audience: "Adults",
     accessibility: "ASL Interpretation Offered",
-    image: "./assets/asl.jpg"
+    image: "./assets/asl.jpg",
+    altText: "ASL mental health service in Baltimore"
   },
   {
     name: "NAMI Metro Baltimore – Spanish-Language Peer Support",
     type: "Peer Support",
     audience: "Family",
     accessibility: "Services Offered in Spanish",
-    image: "./assets/spanish-speaking.jpg"
+    image: "./assets/spanish-speaking.jpg",
+    altText: "Spanish Speaking mental health support in Baltimore"
   },
    { 
     name: "Sarah's House",
     type: "Therapy",
     audience: "Family",
     accessibility: "Hybrid",
-    image: "./assets/sarahs-house.jpg"
+    image: "./assets/sarahs-house.jpg",
+    altText: "Sara's House mental health center in Baltimore"
   }
 ];
