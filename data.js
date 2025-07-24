5eaedf3a0c897e1e4a4826694132893ea7b8f762
Alex Walker <walkernalex@gmail.com>
2025-07-24 08:56:40 -0400

    style: update program card styles and add thumbnail images to programs

--- data.js
+++ data.js
@@ -4,97 +4,97 @@ const programs = [
     type: "Peer Support",
     audience: "Adults",
     accessibility: "Hybrid",
-    image: "https://placehold.co/600x400"
+    image: "./assets/nami-adults.jpg"
   },
   {
     name: "NAMI Metro Baltimore – Peer-led Support Groups",
     type: "Peer Support",
     audience: "Family",
     accessibility: "Hybrid",
-    image: "https://placehold.co/600x400"
+    image: "./assets/nami-family.jpg"
   },
   {
     name: "NAMI Metro Baltimore – Peer-led Support Groups",
     type: "Peer Support",
     audience: "Youth",
     accessibility: "Hybrid",
-    image: "https://placehold.co/600x400"
+    image: "./assets/nami-youth.jpg"
   },
   {
     name: "NAMI Metro Baltimore – LGBTQIA+ Resources & Support",
     type: "Peer Support",
     audience: "LGBTQ",
     accessibility: "Hybrid",
-    image: "https://placehold.co/600x400"
+   image: "./assets/nami-peer-lgbtq.jpg"
   },
   {
     name: "Baltimore County Health – Mobile Crisis and Urgent Care",
     type: "Hotline",
     audience: "Adults",
     accessibility: "In-Person",
-    image: "https://placehold.co/600x400"
+   image: "./assets/hotline.jpg"
   },
   {
     name: "Baltimore County Health – Mobile Crisis and Urgent Care",
     type: "Hotline",
     audience: "Family",
     accessibility: "In-Person",
-    image: "https://placehold.co/600x400"
+    image: "./assets/hotline-2.jpg"
   },
   {
     name: "Hearts & Ears, Inc. (Y2Connect)",
     type: "Peer Support",
     audience: "LGBTQ",
     accessibility: "Hybrid",
-    image: "https://placehold.co/600x400"
+    image: "./assets/hearts-and-ears.jpg"
   },
   {
     name: "Free Baltimore",
     type: "Therapy",
     audience: "LGBTQ",
     accessibility: "In-Person",
-    image: "https://placehold.co/600x400"
+    image: "./assets/free-baltimore.jpg"
   },
   {
     name: "Mighty Behavioral Health Services",
     type: "Therapy",
     audience: "Adults",
     accessibility: "Virtual",
-    image: "https://placehold.co/600x400"
+    image: "./assets/mightyjpg"
   },
   {
     name: "Talkspace Go via Baltimore County Schools",
     type: "Therapy",
     audience: "Teens",
     accessibility: "Virtual",
-    image: "https://placehold.co/600x400"
+    image: "./assets/bcps-talkspacejpg"
   },
   {
     name: "Overcare Pyschiatry and Therapy",
     type: "Therapy",
     audience: "Family",
     accessibility: "In-Person",
-    image: "https://placehold.co/600x400"
+    image: "./assets/overcare.jpg"
   },
   {
     name: "BHSB – ASL-Accessible Services",
     type: "Peer Support",
     audience: "Adults",
     accessibility: "ASL Interpretation Offered",
-    image: "https://placehold.co/600x400"
+    image: "./assets/asl.jpg"
   },
   {
     name: "NAMI Metro Baltimore – Spanish-Language Peer Support",
     type: "Peer Support",
     audience: "Family",
     accessibility: "Services Offered in Spanish",
-    image: "https://placehold.co/600x400"
+    image: "./assets/spanish-speaking.jpg"
   },
    { 
     name: "Sarah's House",
     type: "Therapy",
     audience: "Family",
     accessibility: "Hybrid",
-    image: "https://placehold.co/600x400"
+    image: "./assets/sarahs-house.jpg"
   }
 ];
